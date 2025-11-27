2084f15ea52e13bbb01526a2e82f9284cbf2def6
Sam Price <samprice@Sams-MacBook-Pro.local>
2025-11-27 10:27:23 +0000

    inital commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Docstring for main
+"""
+
+
+def add(x: int, y: int) -> int:
+    """adds 2 numbers"""
+
+    return x + y
---
+++ test_main.py
@@ -0,0 +1,6 @@
+
+from main import add
+
+
+def test_add_1():
+    assert add(3, 4) == 7
